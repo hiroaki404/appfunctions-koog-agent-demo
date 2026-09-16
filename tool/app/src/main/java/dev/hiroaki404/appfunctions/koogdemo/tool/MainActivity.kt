5244974c943c7be67b00f5d36940fde1dc0025c8
hiroaki404 <48251111+hiroaki404@users.noreply.github.com>
2026-09-17 00:32:51 +0900

    create project from template

---
+++ tool/app/src/main/java/dev/hiroaki404/appfunctions/koogdemo/tool/MainActivity.kt
@@ -0,0 +1,47 @@
+package dev.hiroaki404.appfunctions.koogdemo.tool
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.hiroaki404.appfunctions.koogdemo.tool.ui.theme.ToolTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToolTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ToolTheme {
+        Greeting("Android")
+    }
+}
